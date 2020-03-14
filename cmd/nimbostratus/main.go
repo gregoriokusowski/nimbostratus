78cfe2630ebe0e9544da1024ad3f8c1d224b80ff
Gregorio Kusowski <gregorio.kusowski@gmail.com>
2020-03-14 19:13:09 +0100

    Output zones in a readable way

--- cmd/nimbostratus/main.go
+++ cmd/nimbostratus/main.go
@@ -7,9 +7,14 @@ import (
 	"kusowski.com/nimbostratus/aws"
 )
 
+const BASE_FORMAT = "%-28s %-16s"
+const HEADER_FORMAT = BASE_FORMAT + "%10s\n"
+const ZONE_FORMAT = BASE_FORMAT + "[%6dms]\n"
+
 func main() {
+	fmt.Printf(HEADER_FORMAT, "Zone", "Id", "Ping")
 	zones := aws.GetZones(context.TODO())
 	for _, zone := range zones {
-		fmt.Println("%+v", zone)
+		fmt.Printf(ZONE_FORMAT, zone.Name, zone.Id, zone.LatencyInMillis)
 	}
 }
